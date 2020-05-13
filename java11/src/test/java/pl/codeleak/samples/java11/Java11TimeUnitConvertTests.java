d2e5c586095a866a1a15fdc65c8ec9b0e703f57e
rafal.borowiec <rafal.borowiec@gmail.com>
2020-05-13 23:25:02 +0200

    Convert time duration to unit - more examples (Java 11)

--- java11/src/test/java/pl/codeleak/samples/java11/Java11TimeUnitConvertTests.java
+++ java11/src/test/java/pl/codeleak/samples/java11/Java11TimeUnitConvertTests.java
@@ -1,20 +1,35 @@
 package pl.codeleak.samples.java11;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class Java11TimeUnitConvertTests {
 
-    @Test
-    void timeUnitConvertDuration() {
-        TimeUnit unit = TimeUnit.DAYS;
+    @ParameterizedTest
+    @MethodSource("timeUnitToDuration")
+    void convertTimeUnitToDuration(TimeUnit unit, Duration duration, long expectedResult) {
+        long actualResult = unit.convert(duration);
 
-        long result = unit.convert(Duration.ofHours(48));
+        assertThat(actualResult).isEqualTo(expectedResult);
+    }
 
-        assertThat(result).isEqualTo(2);
+    private static Stream<Arguments> timeUnitToDuration() {
+        return Stream.of(
+                Arguments.of(TimeUnit.DAYS, Duration.ofHours(24), 1), // 24h = 1d
+                Arguments.of(TimeUnit.DAYS, Duration.ofHours(40), 1), // 40h = 1d
+                Arguments.of(TimeUnit.DAYS, Duration.ofHours(48), 2), // 48h = 2d
+                Arguments.of(TimeUnit.DAYS, Duration.ofHours(50), 2),  // 50h = 2d
+                Arguments.of(TimeUnit.MILLISECONDS, Duration.ofSeconds(1), 1000),  // 1s = 1000ms
+                Arguments.of(TimeUnit.SECONDS, Duration.ofHours(1), 3600),  // 1h = 3600s
+                Arguments.of(TimeUnit.HOURS, Duration.ofSeconds(4_000), 1),  // 4000s = 1h
+                Arguments.of(TimeUnit.HOURS, Duration.ofSeconds(1_0000), 2)  // 10000s = 2h
+        );
     }
 }
